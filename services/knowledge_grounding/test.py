7a06ecb54210101e758f5ee2b96428b25db62e5c
ewan <ivan.stereotekk@gmail.com>
2023-02-16 22:30:34 +0300

    some tests corrected

--- services/knowledge_grounding/test.py
+++ services/knowledge_grounding/test.py
@@ -1,33 +1,35 @@
 import requests
+import pytest
+
+
+url = "http://0.0.0.0:8083/respond"
+
+checked_sentence1 = (
+    "When Mabel visited their home to play the piano, "
+    "she occasionally glimpsed a flitting swirl of white in the next room, "
+    "sometimes even received a note of thanks for calling, but she never actually "
+    "spoke with the reclusive, almost spectral Emily."
+)
+knowledge1 = (
+    "The real-life soap opera behind the publication of Emily Dickinson’s poems\n"
+    "When Mabel visited their home to play the piano, she occasionally glimpsed "
+    "a flitting swirl of white in the next room, sometimes even received a note of "
+    "thanks for calling, but she never actually spoke with the reclusive, almost spectral Emily."
+)
+text1 = "Yeah she was an icon she died in 1886 at the tender age of 55."
+
+checked_sentence2 = "Penguins are a group of aquatic flightless birds."
+knowledge2 = "Penguins are a group of aquatic flightless birds."
+text2 = "Who are penguins?"
+
+history = (
+    "Do you know who Emily Dickson is?\n"
+    'Emily Dickinson? The poet? I do! "Tell all the truth, but tell it slant" '
+    "she once said. Do you like her poetry?"
+)
 
 
 def test_knowledge_grounding():
-    url = "http://0.0.0.0:8083/respond"
-
-    checked_sentence1 = (
-        "When Mabel visited their home to play the piano, "
-        "she occasionally glimpsed a flitting swirl of white in the next room, "
-        "sometimes even received a note of thanks for calling, but she never actually "
-        "spoke with the reclusive, almost spectral Emily."
-    )
-    knowledge1 = (
-        "The real-life soap opera behind the publication of Emily Dickinson’s poems\n"
-        "When Mabel visited their home to play the piano, she occasionally glimpsed "
-        "a flitting swirl of white in the next room, sometimes even received a note of "
-        "thanks for calling, but she never actually spoke with the reclusive, almost spectral Emily."
-    )
-    text1 = "Yeah she was an icon she died in 1886 at the tender age of 55."
-
-    checked_sentence2 = "Penguins are a group of aquatic flightless birds."
-    knowledge2 = "Penguins are a group of aquatic flightless birds."
-    text2 = "Who are penguins?"
-
-    history = (
-        "Do you know who Emily Dickson is?\n"
-        'Emily Dickinson? The poet? I do! "Tell all the truth, but tell it slant" '
-        "she once said. Do you like her poetry?"
-    )
-
     request_data = {
         "batch": [
             {"checked_sentence": checked_sentence1, "knowledge": knowledge1, "text": text1, "history": history},
