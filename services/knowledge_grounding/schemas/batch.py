a16e8d6259694d91ec33d16bb1f222dfcb5eb118
ewan <ivan.stereotekk@gmail.com>
2023-02-17 00:21:01 +0300

    blind tests created

--- services/knowledge_grounding/schemas/batch.py
+++ services/knowledge_grounding/schemas/batch.py
@@ -1,9 +1,9 @@
-from typing import Tuple
+from typing import Tuple, Union
 from pydantic import BaseModel
 
 
 class Batch(BaseModel):
-    checked_sentence: Tuple = None
-    knowledge: Tuple = None
-    text: str = None
-    history: Tuple = None
+    checked_sentence: Union[Tuple, str, dict] = None
+    knowledge: Union[Tuple, str, dict] = None
+    text: Union[Tuple, str, dict] = None
+    history: Union[Tuple, str, dict] = None
--- services/knowledge_grounding/test.py
+++ services/knowledge_grounding/test.py
@@ -2,44 +2,49 @@ import requests
 import pytest
 
 
-url = "http://0.0.0.0:8083/respond"
+class TestKnowledgeGrounding:
+    url = "http://0.0.0.0:8083/respond"
 
-checked_sentence1 = (
-    "When Mabel visited their home to play the piano, "
-    "she occasionally glimpsed a flitting swirl of white in the next room, "
-    "sometimes even received a note of thanks for calling, but she never actually "
-    "spoke with the reclusive, almost spectral Emily."
-)
-knowledge1 = (
-    "The real-life soap opera behind the publication of Emily Dickinson’s poems\n"
-    "When Mabel visited their home to play the piano, she occasionally glimpsed "
-    "a flitting swirl of white in the next room, sometimes even received a note of "
-    "thanks for calling, but she never actually spoke with the reclusive, almost spectral Emily."
-)
-text1 = "Yeah she was an icon she died in 1886 at the tender age of 55."
+    checked_sentence_one = (
+        "When Mabel visited their home to play the piano, "
+        "she occasionally glimpsed a flitting swirl of white in the next room, "
+        "sometimes even received a note of thanks for calling, but she never actually "
+        "spoke with the reclusive, almost spectral Emily."
+    )
+    knowledge_one = (
+        "The real-life soap opera behind the publication of Emily Dickinson’s poems\n"
+        "When Mabel visited their home to play the piano, she occasionally glimpsed "
+        "a flitting swirl of white in the next room, sometimes even received a note of "
+        "thanks for calling, but she never actually spoke with the reclusive, almost spectral Emily."
+    )
+    text_one = "Yeah she was an icon she died in 1886 at the tender age of 55."
 
-checked_sentence2 = "Penguins are a group of aquatic flightless birds."
-knowledge2 = "Penguins are a group of aquatic flightless birds."
-text2 = "Who are penguins?"
+    checked_sentence_two = "Penguins are a group of aquatic flightless birds."
+    knowledge_two = "Penguins are a group of aquatic flightless birds."
+    text_two = "Who are penguins?"
 
-history = (
-    "Do you know who Emily Dickson is?\n"
-    'Emily Dickinson? The poet? I do! "Tell all the truth, but tell it slant" '
-    "she once said. Do you like her poetry?"
-)
+    history = (
+        "Do you know who Emily Dickson is?\n"
+        'Emily Dickinson? The poet? I do! "Tell all the truth, but tell it slant" '
+        "she once said. Do you like her poetry?"
+    )
 
+    queries = [
+        {"batch":
+            {"checked_sentence": checked_sentence_two, "knowledge": knowledge_two, "text": text_two, "history": history}
+         },
+        {"batch":
+            {"checked_sentence": checked_sentence_one, "knowledge": knowledge_one, "text": text_one, "history": history}},
+        {"batch":
+            {"checked_sentence": None, "knowledge": None, "text": None, "history": None}}
+    ]
 
-def test_knowledge_grounding():
-    request_data = {
-        "batch": [
-            {"checked_sentence": checked_sentence1, "knowledge": knowledge1, "text": text1, "history": history},
-            {"checked_sentence": checked_sentence2, "knowledge": knowledge2, "text": text2, "history": history},
-        ]
-    }
-    results = requests.post(url, json=request_data).json()
-    assert all(results), f"Got empty string among results"
-    print("Got\n{}\nSuccess".format(results))
-
-
-if __name__ == "__main__":
-    test_knowledge_grounding()
+    @pytest.mark.parametrize("queries", queries)
+    def test_knowledge_grounding(self, query):
+        response = requests.post(self.url, json=query).json()
+        if query['batch']['checked_sentence'] == None:
+            assert query['batch']['checked_sentence'] == None, "The checked_sentence should be None"
+            print(f"Got Success with None query {response.text}")
+        else:
+            assert response.status_code == 200 , f"{response.status_code}"
+            print(f"Got successfull with status code 200 {response.text}")
